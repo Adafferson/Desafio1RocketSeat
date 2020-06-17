54bc8706d70c999a91a25ea7d68969ee3be1e9d9
Adafferson Arllen <arllen@MacBook-Pro-de-Adafferson.local>
2020-06-17 16:50:27 -0400

    Inserindo um projeto no github

---
+++ index.js
@@ -0,0 +1,19 @@
+// DESAFIO ROCKEATSEAT
+// Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa.
+
+const Empresa = 
+    {
+        Nome:'RockeatSeat',
+        Cor:'Roxo',
+        Foco:'Programação',
+        Endereço:'Rua Guilherme Gembala',
+        Numero: 260
+}
+
+
+    //Imprima em tela utilizando console.log o nome da empresa e seu endereço no seguinte formato
+   //A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260
+
+
+console.log(`A empresa ${Empresa.Nome} está localizada em ${Empresa.Endereço} Numero ${Empresa.Numero} `)
+
